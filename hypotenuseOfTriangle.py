f87c8e6bbbfa7655d5796814f9a3e8e798844bf5
Carole Mrad <40326122+cmrad@users.noreply.github.com>
2019-01-21 21:25:07 -0700

    Delete hypotenuseOfTriangle.py

--- hypotenuseOfTriangle.py
+++
@@ -1,7 +0,0 @@
-#Let’s try side lengths of 3 and 4, and
-#see what our program generates for the hypotenuse:
-
-side1 = 3
-side2 = 4
-hypot = ((side1 ** 2) + (side2 ** 2)) ** 0.5
-print ('Side 1 is', side1, ' Side 2 is', side2, ' Hypotenuse is:', hypot)
